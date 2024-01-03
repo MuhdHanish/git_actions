8a3ab457765098dbda3e8bf6fa00006fbded3ec4
MuhdHanish <muhammedhanish11@gmail.com>
2024-01-03 21:45:12 +0530

    feat(server): implemtned server in index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const hostname = `127.0.0.1`;
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    const jsonResponse = { message: 'Hello World', time: new Date() };
+    res.end(JSON.stringify(jsonResponse));
+});
+server.listen(port, hostname, () => console.log(`Server running at http://${hostname}:${port}`));

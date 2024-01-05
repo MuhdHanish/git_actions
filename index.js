5622a5c332b319ca717f58a5f9fdfa22873cbc15
MuhdHanish <muhammedhanish11@gmail.com>
2024-01-05 16:29:28 +0530

    feat(localtime): added localtime string with options

--- index.js
+++ index.js
@@ -6,7 +6,20 @@ const port = 8000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
-    const jsonResponse = { message: 'Hello World', time: new Date() };
+
+    const serverTime = new Date();
+    const options = {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        timeZoneName: 'short'
+    };
+    const formattedTime = serverTime.toLocaleString('en-US', options);
+    
+    const jsonResponse = { message: 'Hello World', serverTime: formattedTime };
     res.end(JSON.stringify(jsonResponse));
 });
 server.listen(port, hostname, () => console.log(`Server running at http://${hostname}:${port}`));

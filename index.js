1630bf4eba7f2aff996ae4ebbdce3e2f9258340c
MuhdHanish <muhammedhanish11@gmail.com>
2024-01-05 16:20:41 +0530

    test(server): 1 test successfully completed, reverted in to old  server

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 8000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
-    const jsonResponse = { message: 'Hello World' };
+    const jsonResponse = { message: 'Hello World', time: new Date() };
     res.end(JSON.stringify(jsonResponse));
 });
 server.listen(port, hostname, () => console.log(`Server running at http://${hostname}:${port}`));
